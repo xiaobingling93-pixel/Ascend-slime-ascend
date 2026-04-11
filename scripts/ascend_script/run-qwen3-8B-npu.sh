8554a84b18401ec8b97aaa083cec838e5899b531
cuixiaojin <cuixiaojin@huawei.com>
2026-04-11 18:08:34 +0800

    add qwen3-8b-npu training script
    Co-authored-by: kip-cxj<cuixiaojin@huawei.com>

--- scripts/ascend_script/run-qwen3-8B-npu.sh
+++ scripts/ascend_script/run-qwen3-8B-npu.sh
@@ -9,71 +9,70 @@ pkill -9 python
 sleep 3
 pkill -9 ray
 pkill -9 python
+pkill -9 redis
 
 set -ex
+ulimit -n 65535
 
 # will prevent ray from buffering stdout/stderr
 export PYTHONBUFFERED=16
 export PYTHONPATH="/path/to/Megatron-LM/:/path/to/sglang/python:$PYTHONPATH"
-export PYTORCH_NPU_ALLOC_CONF=expandable_segments:True
-
-export HYDRA_FULL_ERROR=1
-export CUDA_DEVICE_MAX_CONNECTIONS=1
-
-export RAY_EXPERIMENTAL_NOSET_ASCEND_RT_VISIBLE_DEVICES=1
-export RAY_DEBUG=1
-export RAY_DEDUP_LOGS=0
-
 export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+export RAY_EXPERIMENTAL_NOSET_ASCEND_RT_VISIBLE_DEVICES=1
 export HCCL_HOST_SOCKET_PORT_RANGE=60000-60050
 export HCCL_NPU_SOCKET_PORT_RANGE=61000-61050
+export HYDRA_FULL_ERROR=1
+export PYTORCH_NPU_ALLOC_CONF=expandable_segments:True
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")/.." &>/dev/null && pwd)"
-source "${SCRIPT_DIR}/models/glm4.7-30B-A3B.sh"
+source "${SCRIPT_DIR}/models/qwen3-8B.sh"
 
 CKPT_ARGS=(
-   --hf-checkpoint /path/to/models/GLM-4.7-Flash
-   --ref-load /path/to/models/GLM-4.7-Flash_torch_dist
-   --load /path/to/models/GLM-4.7-Flash_slime/
-   --save /path/to/models/GLM-4.7-Flash_slime/
-   --save-interval 100
+   --hf-checkpoint /path/to/Qwen3-8B
+   --ref-load /path/to/Qwen3-8B.sh_torch_dist
+   --load /path/to/Qwen3-8B_slime/
+   --save /path/to/Qwen3-8B_slime/
+   --save-interval 20
 )
 
 ROLLOUT_ARGS=(
-   --prompt-data /path/to/datasets/dapo-math-17k/dapo-math-17k.jsonl
+   --prompt-data /path/to/dapo-math-17k/dapo-math-17k.jsonl
    --input-key prompt
    --label-key label
    --apply-chat-template
    --rollout-shuffle
    --rm-type deepscaler
    --num-rollout 3000
-   --rollout-batch-size 8
+   --rollout-batch-size 32
    --n-samples-per-prompt 8
-   --rollout-max-response-len $((1024 * 4))
-   --rollout-temperature 1
-   --global-batch-size 64
+   --rollout-max-response-len 4096
+   --rollout-temperature 0.8
+   --global-batch-size 256
    --balance-data
 )
 
 EVAL_ARGS=(
    --eval-interval 20
-   --eval-prompt-data aime /path/to/datasets/aime-2024/aime-2024.jsonl
-   --n-samples-per-eval-prompt 1
+   --eval-prompt-data aime /path/to/aime-2024/aime-2024.jsonl
+   --n-samples-per-eval-prompt 16
    --eval-max-response-len 16384
-   --eval-top-p 1
+   --eval-top-p 0.7
 )
 
 PERF_ARGS=(
-   --tensor-model-parallel-size 1
+   --tensor-model-parallel-size 2
+   --sequence-parallel
    --pipeline-model-parallel-size 1
    --context-parallel-size 1
-   --expert-model-parallel-size 8
+   --expert-model-parallel-size 1
    --expert-tensor-parallel-size 1
    --recompute-granularity full
    --recompute-method uniform
    --recompute-num-layers 1
    --use-dynamic-batch-size
-   --max-tokens-per-gpu 8192
+   --max-tokens-per-gpu 20480
+   --no-gradient-accumulation-fusion
 )
 
 GRPO_ARGS=(
@@ -101,20 +100,22 @@ OPTIMIZER_ARGS=(
 WANDB_ARGS=(
    #--use-wandb
    # --wandb-project slime-dev
-   # --wandb-group glm4.7-flash
+   # --wandb-group qwen3-8B-test
+   # --wandb-key ${WANDB_KEY}
 )
 
 SGLANG_ARGS=(
-   --rollout-num-gpus-per-engine 8
-   --sglang-mem-fraction-static 0.7
-   --sglang-enable-dp-attention
-   --sglang-dp-size 8
-   --sglang-enable-dp-lm-head
-   --sglang-moe-dense-tp-size 1
-   --sglang-cuda-graph-max-bs 16
-   --sglang-max-running-requests 64
-   # ======================= NPU 添加参数 =======================
+   --rollout-num-gpus-per-engine 2
+   --sglang-mem-fraction-static 0.8
+   --sglang-cuda-graph-bs 4 8 16 32 64 128
    --sglang-device npu
+   --sglang-disable-radix-cache
+   --sglang-chunked-prefill-size 32768
+   --sglang-max-prefill-tokens 4000
+   --sglang-max-total-tokens 327680
+   --sglang-enable-dp-attention
+   --sglang-enable-dp-lm-head
+   --sglang-attention-backend ascend
 )
 
 MISC_ARGS=(
@@ -126,23 +127,26 @@ MISC_ARGS=(
    --attention-softmax-in-fp32
    # need to comment this when using model with MLA
    --attention-backend flash
-   --moe-token-dispatcher-type alltoall
+   --use-flash-attn
 )
-
 
 # launch the master node of ray in container
 export MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
-ray start --head --node-ip-address ${MASTER_ADDR} --disable-usage-stats --dashboard-host=0.0.0.0 --dashboard-port=8265
+RAY_DEBUG=legacy \
+ray start --head --node-ip-address ${MASTER_ADDR} --disable-usage-stats --dashboard-host=0.0.0.0 --dashboard-port=8265 --ray-debugger-external
 
 # Build the runtime environment JSON with proper variable substitution
 RUNTIME_ENV_JSON="{
   \"env_vars\": {
-    \"PYTHONPATH\": \"/path/to/Megatron-LM/:/path/to/Megatron-Bridge/src:$PYTHONPATH\",
     \"CUDA_DEVICE_MAX_CONNECTIONS\": \"1\",
-    \"ASCEND_TOOLKIT_HOME\": \"/path/to/cann/\",
-    \"ASCEND_OPP_PATH\": \"/path/to/cann/\",
-    \"ASCEND_AICPU_PATH\": \"/path/to/cann/\",
-    \"ASCEND_HOME_PATH\": \"/path/to/cann/\"
+    \"RAY_EXPERIMENTAL_NOSET_ASCEND_RT_VISIBLE_DEVICES\": \"1\",
+    \"ASCEND_TOOLKIT_HOME\": \"/path/to/ascend-toolkit/latest/\",
+    \"ASCEND_OPP_PATH\": \"/path/to/ascend-toolkit/latest/opp/\",
+    \"ASCEND_AICPU_PATH\": \"/path/to/ascend-toolkit/latest/\",
+    \"ASCEND_HOME_PATH\": \"/path/to/ascend-toolkit/latest/\",
+    \"set_env_path\": \"/path/to/nnal/atb/set_env.sh\",
+    \"HYDRA_FULL_ERROR\": \"1\",
+    \"RAY_DEBUG_POST_MORTEM\": \"1\"
   }
 }"
 
@@ -152,15 +156,13 @@ ray job submit --address="http://127.0.0.1:8265" \
    --actor-num-nodes 1 \
    --actor-num-gpus-per-node 8 \
    --rollout-num-gpus 8 \
-   --num-gpus-per-node 8 \
    ${MODEL_ARGS[@]} \
+   ${CKPT_ARGS[@]} \
    ${ROLLOUT_ARGS[@]} \
    ${OPTIMIZER_ARGS[@]} \
    ${GRPO_ARGS[@]} \
    ${WANDB_ARGS[@]} \
    ${PERF_ARGS[@]} \
+   ${EVAL_ARGS[@]} \
    ${SGLANG_ARGS[@]} \
-   ${MISC_ARGS[@]} \
-   ${SPEC_ARGS[@]} \
-   ${CKPT_ARGS[@]} \
-   ${EVAL_ARGS[@]}
+   ${MISC_ARGS[@]}
